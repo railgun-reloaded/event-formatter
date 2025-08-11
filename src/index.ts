26e16f46fee3227ae30cfa1b35f9ebf64a70e410
Itachi546 <28756796+Itachi546@users.noreply.github.com>
2025-08-11 09:58:15 +0545

    Add import/export in index.ts

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import { formatCommitmentBatchEvent, formatGeneratedCommitmentBatchEvent, formatNullifiedEvent } from './events-v1'
+import { formatNullifiedEventV2, formatShieldEventV2, formatTransactEventV2, formatUnshieldEventV2 } from './events-v2'
+import { formatShieldEventV2_1 } from './events_v2_1'
+
+export {
+  formatCommitmentBatchEvent, formatGeneratedCommitmentBatchEvent, formatNullifiedEvent,
+  formatNullifiedEventV2, formatShieldEventV2, formatTransactEventV2, formatUnshieldEventV2,
+  formatShieldEventV2_1
+}
